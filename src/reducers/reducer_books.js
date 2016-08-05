55226bf2c35b528dd9727132db433f2727349203
Michael Scragg <michael.scragg@dsisolutions.biz>
2016-08-05 16:41:13 -0500

    Initial Commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,7 @@
+export default function() {
+  return [
+    { title: 'Javascript: The Good Parts', pages: 345 },
+    { title: 'Harry Potter', pages: 12 },
+    { title: 'Dune ', pages: 25 }
+  ]
+}
